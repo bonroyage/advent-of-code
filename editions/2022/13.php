7ed5966298d558d14e44a304100dfb48eb393729
Roy de Vos Burchart <bonroyage@users.noreply.github.com>
2022-12-26 14:39:57 +0100

    2022, day 13

---
+++ editions/2022/13.php
@@ -0,0 +1,116 @@
+<?php
+
+use App\Solver\Day;
+use App\Solver\Part;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+return new class ('') extends Day {
+
+    public function handle(): Generator
+    {
+        yield $this->part1();
+        yield $this->part2();
+    }
+
+
+    private function input(): Collection
+    {
+        return $this->readFile()
+            ->filter()
+            ->map(fn($packet) => json_decode($packet, true));
+    }
+
+
+    public function part1(): Part
+    {
+        $packets = $this->input()->chunk(2)->map->values();
+
+        $correctPackets = [];
+
+        foreach ($packets as $index => $packet) {
+            if ($this->compare($packet[0], $packet[1])) {
+                $correctPackets[] = $index + 1; // Packets are 1-indexed
+            }
+        }
+
+        return new Part(
+            question: 'Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?',
+            answer: array_sum($correctPackets),
+        );
+    }
+
+
+    public function part2(): Part
+    {
+        $packets = $this->input();
+        $packets[] = [[2]];
+        $packets[] = [[6]];
+
+        $packets = $packets->sort(function ($left, $right) {
+            $cmp = $this->compare($left, $right);
+
+            if ($cmp === true) {
+                return -1;
+            } else if ($cmp === false) {
+                return 1;
+            }
+
+            return 0;
+        })->values();
+
+        $firstDivider = $packets->search([[2]]) + 1;
+        $secondDivider = $packets->search([[6]]) + 1;
+
+        return new Part(
+            question: 'Organize all of the packets into the correct order. What is the decoder key for the distress signal?',
+            answer: $firstDivider * $secondDivider,
+        );
+    }
+
+
+    private function compare(array|int|null $left, array|int|null $right): ?bool
+    {
+        // If the left list runs out of items first, the inputs are in the right order.
+        if ($left === null && $right !== null) {
+            return true;
+        }
+
+        // If the right list runs out of items first, the inputs are not in the right order.
+        if ($right === null && $left !== null) {
+            return false;
+        }
+
+        // If both values are integers, the lower integer should come first.
+        if (is_int($left) && is_int($right)) {
+            // If the left integer is lower than the right integer, the inputs are in the right order.
+            if ($left < $right) {
+                return true;
+            }
+
+            // If the left integer is higher than the right integer, the inputs are not in the right order.
+            if ($left > $right) {
+                return false;
+            }
+
+            // Otherwise, the inputs are the same integer; continue checking the next part of the input.
+            return null;
+        }
+
+        $left = Arr::wrap($left);
+        $right = Arr::wrap($right);
+
+        for ($i = 0; ; $i++) {
+            // If the lists are the same length and no comparison makes a decision
+            // about the order, continue checking the next part of the input.
+            if (!isset($left[$i]) && !isset($right[$i])) {
+                return null;
+            }
+
+            if (($res = $this->compare($left[$i] ?? null, $right[$i] ?? null)) !== null) {
+                return $res;
+            }
+        }
+    }
+
+};
