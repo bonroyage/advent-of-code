45400b86333ad0b799ff4800ec07f01f373e27da
Roy de Vos Burchart <bonroyage@users.noreply.github.com>
2024-12-07 23:10:39 +0100

    ⚡️ 2024, day 7

--- editions/2024/Day7/solver.php
+++ editions/2024/Day7/solver.php
@@ -14,7 +14,7 @@ return new class('Bridge Repair') extends Day
 
                 return [
                     'target' => (int) $matches[1],
-                    'values' => explode(' ', $matches[2]),
+                    'values' => array_map(fn($v) => (int) $v, explode(' ', $matches[2])),
                 ];
             });
     }
@@ -24,20 +24,12 @@ return new class('Bridge Repair') extends Day
     {
         $input = $this->input();
 
-        $values = $input
-            ->filter(function (array $line) {
-                return $this->calculate(
-                    carry: array_shift($line['values']),
-                    values: $line['values'],
-                    target: $line['target'],
-                    operations: static function (int $carry, int $value) {
-                        yield $carry + $value;
-                        yield $carry * $value;
-                    },
-                );
-            });
-
-        return $values->sum('target');
+        return $input
+            ->filter(fn(array $line) => $this->calculate(
+                carry: $line['target'],
+                values: $line['values'],
+            ))
+            ->sum('target');
     }
 
     #[SampleAnswer(11387)]
@@ -45,41 +37,72 @@ return new class('Bridge Repair') extends Day
     {
         $input = $this->input();
 
-        $values = $input
-            ->filter(function (array $line) {
-                return $this->calculate(
-                    carry: array_shift($line['values']),
-                    values: $line['values'],
-                    target: $line['target'],
-                    operations: static function (int $carry, int $value) {
-                        yield $carry + $value;
-                        yield $carry * $value;
-                        yield (int) "{$carry}{$value}";
-                    },
-                );
-            });
-
-        return $values->sum('target');
+        return $input
+            ->filter(fn(array $line) => $this->calculate(
+                carry: $line['target'],
+                values: $line['values'],
+                withConcatenation: true,
+            ))
+            ->sum('target');
     }
 
-    public function calculate(int $carry, array $values, int $target, Closure $operations): bool
+    public function calculate(int $carry, array $values, bool $withConcatenation = false): bool
     {
         if ($values === []) {
-            return $carry === $target;
+            return $carry === 0;
         }
 
-        $value = array_shift($values);
+        $value = array_pop($values);
 
-        foreach ($operations($carry, $value) as $calculatedOperation) {
-            if ($calculatedOperation > $target) {
-                continue;
-            }
+        /*
+         * ADDITION
+         *
+         * We can only perform addition if the carry minus the value is at least
+         * zero.
+         */
 
-            if ($this->calculate($calculatedOperation, $values, $target, $operations)) {
-                return true;
-            }
+        $subtracted = $carry - $value;
+
+        if ($subtracted >= 0 && $this->calculate($subtracted, $values, $withConcatenation)) {
+            return true;
         }
 
-        return false;
+        /*
+         * MULTIPLICATION
+         *
+         * We can only perform multiplication if the carry is divisible by the
+         * value.
+         */
+
+        if ($carry % $value === 0 && $this->calculate($carry / $value, $values, $withConcatenation)) {
+            return true;
+        }
+
+        /*
+         * CONCATENATION
+         *
+         * We can definitely rule out concatenation if the value is not less than
+         * the carry value.
+         *
+         * We can also rule it out if the carry value does not end with the value.
+         */
+
+        if (!$withConcatenation) {
+            return false;
+        }
+
+        if ($value >= $carry) {
+            return false;
+        }
+
+        $multiplier = pow(10, ceil(log10($value + 1)));
+
+        if ($carry % $multiplier !== $value) {
+            return false;
+        }
+
+        $split = ($carry - $value) / $multiplier;
+
+        return $this->calculate($split, $values, true);
     }
 };
